e48f144002af78f34d3e81e842ad9cf6f4ae420a
Vaughn-Mina <vonzkymina@yahoo.com>
2023-03-13 11:31:47 -0700

    testing testing

---
+++ settings.py
@@ -0,0 +1,20 @@
+# File created by Vaughn Mina
+
+# screen dimensions
+WIDTH = 800
+HEIGHT = 800
+
+# game settings:
+FPS = 30
+RUNNING = True
+# player attributes
+PLAYER_ACC = 2
+MOB_ACC = 2
+PLAYER_FRICTION = -0.12
+MOB_FRICTION = -0.13
+MOB_REVERSE_FRICTION = 0.14
+
+# colors
+BLACK = (0,0,0)
+BLUE = (50,50,255)
+RED = (255, 50, 50)
